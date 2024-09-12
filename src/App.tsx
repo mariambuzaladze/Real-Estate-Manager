f3015e393cec20ebe95be284f5e61236bb853827
mariambuzaladze <mariambuzaladze10@example.come>
2024-09-12 11:08:04 +0400

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import ListingPage from "./pages/ListingPage";
 import AddListing from "./pages/AddListing";
@@ -7,6 +8,7 @@ import AddListing from "./pages/AddListing";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/listing/:id" element={<ListingPage />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import RedberryLogo from "/images/redberry logo.png";
+
+export default function Header() {
+  return (
+    <div className="border border-gray-300 py-10 px-40">
+      <img src={RedberryLogo} alt="redberry logo" />
+    </div>
+  );
+}
