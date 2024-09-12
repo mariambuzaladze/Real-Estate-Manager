f34293c7b6a2383a7fd6ddcd53d9f73b75980389
mariambuzaladze <mariambuzaladze10@example.come>
2024-09-12 10:54:40 +0400

    feat: add page and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import ListingPage from "./pages/ListingPage/ListingPage";
+import AddListing from "./pages/AddListing/AddListing";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listing/:id" element={<ListingPage />} />
+        <Route path="/addListing" element={<AddListing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
